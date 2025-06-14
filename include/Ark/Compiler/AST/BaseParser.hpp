ee20c644f594bb34b993f723a221049c170e2088
27justin <me@justin.cx>
2025-06-14 14:26:22 +0200

    add: ci workflow for mingw

--- include/Ark/Compiler/AST/BaseParser.hpp
+++ include/Ark/Compiler/AST/BaseParser.hpp
@@ -84,7 +84,7 @@ namespace Ark::internal
          *
          * @return distance in characters from the beginning of the file to the cursor
          */
-        ssize_t getCount() { return std::distance(m_str.begin(), m_it); }
+        intptr_t getCount() { return std::distance(m_str.begin(), m_it); }
 
         /**
          *
